3a73f22395a0b14126d561d93149bb1f84df01bf
Cesar Ayuso <csayuso@ucdavis.edu>
2022-03-03 00:31:02 -0800

    started react app for more dyanamic experience

---
+++ clear-lake-restoration-app/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import NavbarComponent from './Components/NavbarComponent';
+function App() {
+  return (
+    <div className="App">
+      <NavbarComponent/>
+    </div>
+  );
+}
+
+export default App;

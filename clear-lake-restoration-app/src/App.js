2f2eb07fd77be04879d740fbcc71d0e245f5d004
Cesar Ayuso <csayuso@ucdavis.edu>
2022-03-09 15:05:43 -0800

    integrated stream charts

--- clear-lake-restoration-app/src/App.js
+++ clear-lake-restoration-app/src/App.js
@@ -1,17 +1,15 @@
 import React, { useRef, useEffect, useState } from 'react';
 import './App.css';
-import Navbar from './Components/Navbar';
-import Title from './Components/Title';
+import Navbar from './components/Navbar';
+import Title from './components/Title';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import Home from './Components/pages/Home.js';
-import Stream from './Components/pages/streams/Stream.js';
-import Kelsey from './Components/pages/streams/Kelsey.js';
-import Middle from './Components/pages/streams/Middle.js';
-import Scotts from './Components/pages/streams/Scotts.js';
-import ContactUs from './Components/pages/ContactUs.js';
-import Met from './Components/pages/met/Met.js';
-import Lake from './Components/pages/lake/Lake.js';
+import Home from './components/pages/Home.js';
+import Stream from './components/pages/streams/Stream.js';
+import Kelsey from './components/pages/streams/Kelsey.js';
+import Middle from './components/pages/streams/Middle.js';
+import StreamHome from './components/pages/streams/StreamHome.js';
+import ContactUs from './components/pages/ContactUs.js';
 
 function getCurrentTime() {
   var time = new Date().toLocaleDateString();
@@ -54,10 +52,10 @@ function App() {
       <Navbar/>
       <Routes>
         <Route path='/' exact element={<Home/>} />
-        <Route path='/stream' exact element={<Stream />} />
-        <Route path='/kelsey' exact element={<Stream fromDate={fromDate} endDate={toDate}/>} />
-        <Route path='/middle' exact element={<Middle/>} />
-        <Route path='/scotts' exact element={<Scotts/>} />
+        <Route path='/stream' exact element={<StreamHome />} />
+        <Route path='/kelsey' exact element={<Stream fromDate={fromDate} endDate={toDate} id={"1"} name={"Kelsey"}/>} />
+        <Route path='/middle' exact element={<Stream fromDate={fromDate} endDate={toDate} id={"2"} name={"Middle"}/>} />
+        <Route path='/scotts' exact element={<Stream fromDate={fromDate} endDate={toDate} id={"3"} name={"Scotts"}/>} />
         <Route path='/contact-us' exact element={<ContactUs/>} />
         <Route path='/met' exact element={<Met/>} />
         <Route path='/lake' exact element={<Lake/>} />
--- clear-lake-restoration-app/src/Components/StreamChart.js
+++ clear-lake-restoration-app/src/Components/StreamChart.js
@@ -116,106 +116,13 @@ function getFilteredData(data, dataType) {
 export default function StreamChart({
     fromDate,
     endDate,
-    id
+    id,
+    dataType,
+    chartProps
 }) {
 
   const chartComponent = useRef(null); 
-  const [chartOptions, setChartOptions] = useState({
-    chart: {
-        zoomType: 'x',
-        height: 700,
-    },
-    subtitle: {
-        text: document.ontouchstart === undefined ?
-            'Click and drag in the plot area to zoom in' : 'Pinch the chart to zoom in'
-    },
-    title: {
-        text: 'Turbity Mean and Flow Chart'
-    },
-    xAxis: {
-        type: 'datetime'
-    },
-    yAxis: 
-    [{ // Primary yAxis
-        labels: {
-            format: '{value} NTU',
-            style: {
-                color: Highcharts.getOptions().colors[3]
-            }
-        },
-        title: {
-            text: 'Turbity [NTU]',
-            style: {
-                color: Highcharts.getOptions().colors[3]
-            }
-        },
-        opposite: true,
-        height: '50%',
-        lineColor: Highcharts.getOptions().colors[3],
-        lineWidth: 5,
-       
-    }, { // Secondary yAxis
-        title: {
-            text: 'Flow [cfs]',
-            style: {
-                color: Highcharts.getOptions().colors[0]
-            }
-        },
-        labels: {
-            format: '{value} cfs',
-            style: {
-                color: Highcharts.getOptions().colors[0]
-            }
-        },
-        height: '50%',
-        top: '50%',
-        lineColor: Highcharts.getOptions().colors[0],
-        lineWidth: 5,
-        
-    }],
-    tooltip: {
-        formatter: function () {
-            // The first returned item is the header, subsequent items are the
-            // points
-            const DayOfMonth = new Date(this.x).getDate();
-            const Month = new Date(this.x).getMonth(); // Be careful! January is 0, not 1
-            const Year = new Date(this.x).getFullYear();
-            const TimeHrs = new Date(this.x).getHours();
-            const TimeMins = new Date(this.x).getMinutes();
-            const dateString = (Month + 1) + "-" + DayOfMonth + "-" + Year + "  " + TimeHrs + ":" + TimeMins;
-            return [dateString].concat(
-                this.points ?
-                    this.points.map(function (point) {
-                        return point.series.name + ': ' + point.y;
-                    }) : []
-            );
-        },
-        split: true
-    },
-
-    series: [
-        {
-            name: 'Turbity',
-            data: [],
-            selected: true,
-            yAxis: 0,
-            color: Highcharts.getOptions().colors[3],
-            
-        }, 
-        {
-            name: 'Flow',
-            data: [],
-            selected: true,
-            yAxis: 1,
-            color: Highcharts.getOptions().colors[0],
-        },
-         
-    ],
-    updateTime: {
-        setTime: 0,
-        endTime: 0,
-    }
-  })
+  const [chartOptions, setChartOptions] = useState(chartProps)
   var url = new URL('https://tepfsail50.execute-api.us-west-2.amazonaws.com/v1/report/cl-creeks');
   var search_params = url.searchParams;
   search_params.set('id',id);
@@ -229,12 +136,12 @@ export default function StreamChart({
   useEffect(()=> {
     console.log(isLoading)
     if (!isLoading) {
-        var turbData = getFilteredData(data,"Turb_BES")
-        console.log(turbData)
+        var filteredData = getFilteredData(data,dataType)
+        console.log(filteredData)
         setChartOptions(()=> ({
             series: [
                 {
-                    data: turbData
+                    data: filteredData
                 }
             ]
         }))

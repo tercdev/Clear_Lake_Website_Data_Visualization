1051b36eb976018b9bc648cba6830cf94dbddbf1
Alison <alisonchen.y@gmail.com>
2022-03-09 00:19:41 -0800

    file location error fixe

--- clear-lake-restoration-app/src/App.js
+++ clear-lake-restoration-app/src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './App.css';
-import Navbar from './components/Navbar';
-import Title from './components/Title';
+import Navbar from './Components/Navbar';
+import Title from './Components/Title';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import Home from './components/pages/Home.js';
-import Stream from './components/pages/streams/Stream.js';
-import Kelsey from './components/pages/streams/Kelsey.js';
-import Middle from './components/pages/streams/Middle.js';
-import Scotts from './components/pages/streams/Scotts.js';
-import ContactUs from './components/pages/ContactUs.js';
+import Home from './Components/pages/Home.js';
+import Stream from './Components/pages/streams/Stream.js';
+import Kelsey from './Components/pages/streams/Kelsey.js';
+import Middle from './Components/pages/streams/Middle.js';
+import Scotts from './Components/pages/streams/Scotts.js';
+import ContactUs from './Components/pages/ContactUs.js';
 
 function App() {
   return (

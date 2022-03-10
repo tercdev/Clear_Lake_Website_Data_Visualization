8dcf86305d925d9cc2e9389e6261584786676685
vtlai88 <vtlai@ucdavis.edu>
2022-03-09 18:18:07 -0800

    added stream map

--- clear-lake-restoration-app/src/Components/pages/streams/StreamHome.js
+++ clear-lake-restoration-app/src/Components/pages/streams/StreamHome.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Map from '../../Map.js'
 
 export default function StreamHome() {
     return (
         <div>
             <h1 className='stream-home'>Stream Monitoring</h1>
+            <Map name="stream"/>
+            
         </div>
     )
 }

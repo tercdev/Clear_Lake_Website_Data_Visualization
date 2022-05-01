cafaeeefd9b32ea676a4d7edbf39c9d41e93684c
vtlai88 <vtlai@ucdavis.edu>
2022-05-01 16:41:12 -0700

    fixed text

--- clear-lake-restoration-app/src/Components/pages/streams/StreamHome.js
+++ clear-lake-restoration-app/src/Components/pages/streams/StreamHome.js
@@ -9,7 +9,7 @@ export default function StreamHome() {
             </div>
             
             <div className='data-desc-container'>
-                <p className='data-desc'>In December 2018, in-situ stream sensors were deployed in the three primary lake tributaries: Kelsey, Middle and Scotts Creek. The probes record stream turbidity and temperature measurements and transmit data every 10 minutes via a cellular modem to UC Davis servers. The sensors are co-located with existing California Department of Water Resources gauging stations. </p>
+                <p className='data-desc'>In December 2018, in-site stream sensors were deployed in the three primary lake tributaries: Kelsey, Middle and Scotts Creek. The probes record stream turbidity and temperature measurements and transmit data every 10 minutes via a cellular modem to UC Davis servers. The sensors are co-located with existing California Department of Water Resources gauging stations. </p>
             </div>
 
             <Map name="stream"/>

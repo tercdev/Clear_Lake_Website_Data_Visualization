adf22472db4dc0048292ac2bb631402e0c63c32b
vtlai88 <vtlai@ucdavis.edu>
2022-05-03 12:55:47 -0700

    added reminders on upload page

--- clear-lake-restoration-app/src/Components/pages/upload-csv/LoginPage.js
+++ clear-lake-restoration-app/src/Components/pages/upload-csv/LoginPage.js
@@ -45,13 +45,51 @@ export default function LoginPage() {
                     <p>*Admin Use Only</p>
                     <p className='drop-disclaimer1'>For all questions, please contact Dr. Alicia Cortes (alicortes@ucdavis.edu)</p>
                 </div>
-                
             </div>
          
             {(user.email != "") 
                 ? ( // when user successfully logs in
                     <div className='welcome-container'>
                         <h2 className="welcome-header">Welcome, <span>{user.name}</span>!</h2> 
+                        <div className="remainder-container">
+                            <h2 className="remainder-header">Naming Convention for file uploads</h2>
+                            <p className="file-type">Meterology Files:</p>
+                            <ul>
+                                <li>met_bkp_FROMdate_TOdate.csv</li>
+                            </ul>
+                            <p className="file-type">Stream Files:</p>
+                            <ul>
+                                <li>stream_kelsey_FROMdate_TOdate.csv</li>
+                            </ul>
+                            <p className="file-type">Lake Files:</p>
+                            <ul>
+                                <li>Profile Data (CTD)</li>
+                                <ul>
+                                    <li>ClearLake_ProfileData_FROMdate_TOdate.csv</li>
+                                </ul>
+                                <li>Lake Mooring (TChain) Oxygen</li>
+                                <ul>
+                                    <li>ClearLake_LA03oxygen_FROMdate_TOdate.csv</li>
+                                </ul>
+                                <li>Lake Mooring (TChain) Temperature</li>
+                                <ul>
+                                    <li>ClearLake_LA03temperature_FROMdate_TOdate.csv</li>
+                                </ul>
+                            </ul>
+                        </div>
+
+                        <div className="remainder-container">
+                            <h2 className="remainder-header">Other remainders</h2>
+                            <ul>
+                                <li>Dropzone only accepts up to 5 CSV files</li>
+                                <li>Files must be in .csv format</li>
+                                <li>If you are uploading files with same name with same FROMdate and same TOdate (but the data contents changed), 
+                                    please add extra unique characters at the end of file name. (Example: add _0, _1, _2 ... at end of filename)</li>
+                                <li>Why do this? The uploaded filenames must be unique to be inserted into database</li>
+                                <li>Once uploaded file, please verify database table on changes</li>
+                            </ul>
+                        </div>.
+                        
                         <Uploader />
                         <div className="logout-btn-container">
                             <button className="logout-btn" onClick={Logout}>Logout</button>

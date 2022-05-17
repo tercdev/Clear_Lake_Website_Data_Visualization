8fe501eb7de2d17eb75505fa31107a47998d16fc
vtlai88 <vtlai@ucdavis.edu>
2022-05-16 18:56:27 -0700

    moved some styling to main App.css

--- clear-lake-restoration-app/src/Components/CollapsibleItem.js
+++ clear-lake-restoration-app/src/Components/CollapsibleItem.js
@@ -2,10 +2,8 @@ import * as React from 'react';
 import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
-// import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
-import "./CollapsibleItem.css"
 
 export default function CollapsibleItem(props) {
   return (
--- clear-lake-restoration-app/src/Components/DataDisclaimer.js
+++ clear-lake-restoration-app/src/Components/DataDisclaimer.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import './DataDisclaimer.css'
-
 function DataDisclaimer() {
     return (
         <div className='data-disclaimer'>

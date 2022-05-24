3b5bf66c13a305ea1f5bc5beb7596b08872a1aa4
Alison <alisonchen.y@gmail.com>
2022-05-23 22:32:41 -0700

    remove date

--- clear-lake-restoration-app/src/App.js
+++ clear-lake-restoration-app/src/App.js
@@ -18,41 +18,7 @@ import LakeCTD from './Components/pages/lakeCTD/LakeCTD.js';
 import DataArchive from './Components/pages/data-archive/DataArchive';
 import Map from './Components/Map.js';
 
-function getCurrentTime() {
-  var time = new Date().toLocaleDateString();
-  time = time.split('/');
-  var currentTimeArr = time.slice(0).reverse().map(
-      val => { return val;
-  })
-  if (currentTimeArr[1].length < 2) {
-      currentTimeArr[1] = '0' + currentTimeArr[1];
-  }
-  if (currentTimeArr[2].length < 2) {
-      currentTimeArr[2] = '0' + currentTimeArr[2];
-  }
-  var curTime = currentTimeArr[0] + currentTimeArr[2] + currentTimeArr[1];
-  return curTime;
-}
-
-function getPreviousWeekDate() {
-  var today = new Date();
-  var lastWeek = new Date(today.getFullYear(), today.getMonth(), today.getDate()-7);
-  var month = (lastWeek.getUTCMonth() + 1).toString(); //months from 1-12
-  var day = lastWeek.getUTCDate().toString();
-  var year = lastWeek.getUTCFullYear().toString();
-
-  if (month.length < 2) {
-      month = '0' + month;
-  }
-  if (day.length < 2) {
-      day = '0' + day;
-  }
-  return year+month+day;
-}
-
 function App() {
-  var fromDate = getPreviousWeekDate();
-  var toDate = getCurrentTime();
   return (
     <Router basename='/Clear_Lake_Website_Data_Visualization/'>
       {window.location.pathname !== '/Clear_Lake_Website_Data_Visualization/map' ? <><FullHeader />

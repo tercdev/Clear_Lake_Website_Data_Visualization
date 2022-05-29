18121efc517f6293f380a42778571d97c61f9746
Alison <alisonchen.y@gmail.com>
2022-05-29 11:08:28 -0700

    add comments

--- clear-lake-restoration-app/src/Components/Chart.js
+++ clear-lake-restoration-app/src/Components/Chart.js
@@ -2,10 +2,17 @@ import React, { useEffect, useRef } from 'react'
 import Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
 
+// for the hamburger button in the top right corner of each chart that lets users download different formats of the chart
 require('highcharts/modules/exporting')(Highcharts);
 require('highcharts/modules/export-data')(Highcharts);
 
-export default function StreamChart({
+/**
+ * Component for showing Highcharts chart
+ * @param {Object} chartProps chart properties object
+ * @param {boolean} isLoading whether data is still being fetched or not
+ * @returns {JSX.Element}
+ */
+export default function Chart({
     chartProps,
     isLoading
 }) {
@@ -13,10 +20,10 @@ export default function StreamChart({
     const chartComponent = useRef(null); 
     const chart = chartComponent.current?.chart;
     useEffect(()=> {
-      if (chart) {
+      if (chart) { // show and hide Loading... text
           chart.showLoading();
           if (!isLoading) {
-              chart.hideLoading()
+              chart.hideLoading();
           }
       }
     },[isLoading])

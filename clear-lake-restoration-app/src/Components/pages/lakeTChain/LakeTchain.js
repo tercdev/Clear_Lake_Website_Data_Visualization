0444f305f6de27fa8002b1cf22e7df6d06e00819
vtlai88 <vtlai@ucdavis.edu>
2022-05-30 17:52:17 -0700

    added local

--- clear-lake-restoration-app/src/Components/pages/lakeTChain/LakeTchain.js
+++ clear-lake-restoration-app/src/Components/pages/lakeTChain/LakeTchain.js
@@ -628,7 +628,7 @@ export default function LakeTchain(props) {
     // for the collapsible FAQ
     const header1 = "How to use the graphs and see the data below?";
     const content1 = [<ol>
-            <li>Select start and end dates with maximum 365-day period. Time is in PST.</li>
+            <li>Select start and end dates with maximum 365-day period. Local time is in PST.</li>
             <li>Click submit to update the graphs below.</li>
             <li>Graph and data loading will depend on the length of the selected time period. For example, longer time periods will result to longer loading times.</li>
             *Note: The white dots on the left side of the charts represent depth of the loggers. The black line on the Dissolved Oxygen graph represents the depth of the water column.
--- clear-lake-restoration-app/src/Components/pages/streams/Stream.js
+++ clear-lake-restoration-app/src/Components/pages/streams/Stream.js
@@ -609,7 +609,7 @@ export default function Stream(props) {
     // for the collapsible FAQ
     const header1 = "How to use the graphs and see the data below?";
     const content1 = [<ol>
-            <li>Select start and end dates with maximum 365-day period. Time is in PST.</li>
+            <li>Select start and end dates with maximum 365-day period. Local time is in PST.</li>
             <li>Click submit to update the graphs below.</li>
             <li>Graph and data loading will depend on the length of the selected time period. For example, longer time periods will result to longer loading times.</li>
             <p>*Note: Clean data is plotted on solid line. Provisional data is plotted on dashed line.</p>
